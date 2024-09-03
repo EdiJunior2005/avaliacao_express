475e2d8a1558e7e788199cadfaf4792850dad42d
EdiJunior2005 <edicfjunior05@gmail.com>
2024-09-03 15:04:38 -0300

    commit incial

---
+++ data.js
@@ -0,0 +1,9 @@
+const livros = [
+    {id: 1, titulo: "Biblia Sagrada", autor: "Deus", ano: 1519, genero: "religioso"}
+]
+const estudante = [
+    {id: 1, nome: "Edi", matricula: 1234879, curso: "lionsdev", ano: 2024}
+]
+const aluguel = [
+    {id: 1, idLivro: 1, idEstudante: 1, dataAluguel: "03-09-2024", dataDevolucao: "10-09-2024"}
+]
