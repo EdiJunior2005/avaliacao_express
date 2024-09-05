040f03c6c29bf8cd34fb026d4c4aa8cf7e73ca8b
Alex30pro <alexandryopg87@gmail.com>
2024-09-05 14:26:44 -0300

    ok

--- aluguel/buscarAluguelPorData.js
+++ aluguel/buscarAluguelPorData.js
@@ -1,10 +1,29 @@
 const { aluguel } = require('../data');
 
-const buscarAluguelPorData = (req, res) => {
-    const { dataluguel } = req.query;
+const formatarData = (data) => {
+    const [dia, mes, ano] = data.split('-');
+    return `${ano}-${mes}-${dia}`;
+};
 
-    const resultados = aluguel.filter(alugueis => alugueis.data.toLowerCase().includes(dataluguel.toLowerCase())
-    );
+const buscarAluguelPorData = (req, res) => {
+    const { dataAluguel } = req.query;
+
+    console.log('Parâmetro dataAluguel recebido:', dataAluguel);
+
+    if (!dataAluguel) {
+        return res.status(400).send({ message: 'Data de aluguel não fornecida.' });
+    }
+
+    const dataFormatada = formatarData(dataAluguel);
+
+    console.log('Data formatada para comparação:', dataFormatada);
+
+    const resultados = aluguel.filter(alugueis => {
+        const dataAluguelFormatada = formatarData(alugueis.dataAluguel);
+        return dataAluguelFormatada === dataFormatada;
+    });
+
+    console.log('Resultados encontrados:', resultados);
 
     if (resultados.length === 0) {
         return res.status(404).send({ message: 'Nenhum aluguel encontrado.' });
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+const express = require("express");
+
+// Importar controladores
 const criarAluguel = require("./aluguel/criarAluguel");
 const editarAluguel = require("./aluguel/editarAluguel");
 const listarAluguel = require("./aluguel/listarAluguel");
 const removerAluguel = require("./aluguel/removerAluguel");
-
-const express = require("express");
 const buscarEstudantePorCurso = require("./estudantes/buscarEstudantePorCurso");
 const buscarEstudantePorMatricula = require("./estudantes/buscarEstudantePorMatricula");
 const buscarEstudantePorNome = require("./estudantes/buscarEstudantePorNome");
+const cadastrarEstudantes = require("./estudantes/cadastrarEstudantes");
+const listarEstudantes = require("./estudantes/listarEstudantes");
+const editarEstudante = require("./estudantes/editarEstudantes");
+const removerEstudantes = require("./estudantes/removerEstudantes");
+const criarLivro = require("./livros/criarLivro");
+const listarLivro = require("./livros/listarLivro");
+const editarLivro = require("./livros/editarLivro");
+const deletarLivro = require("./livros/deletarLivro");
+const buscarLivroPorAno = require("./livros/buscarLivroPorAno");
+const buscarLivroPorAutor = require("./livros/buscarLivroPorAutor");
+const buscarLivroPorGenero = require("./livros/buscarLivroPorGenero");
+const buscarLivroPorTitulo = require("./livros/buscarLivroPorTitulo");
+const buscarAlugueisPorIdEstudante = require("./aluguel/buscarAluguelPorIdDoEstudante");
+const buscarAluguelPorIdLivro = require("./aluguel/buscarAluguelPorIdDoLivro");
+const buscarAluguelPorData = require("./aluguel/buscarAluguelPorData");
+
 const app = express();
 const port = 3000;
+
 app.use(express.json());
 
+// Rotas
 app.get("/", (req, res) => {
-  res.send("seja bem vindo!!");
+  res.send("Seja bem-vindo!!");
 });
 
 app.get("/alugueis", listarAluguel);
@@ -24,49 +43,25 @@ app.get("/estudantes/curso", buscarEstudantePorCurso);
 app.get("/estudantes/matricula", buscarEstudantePorMatricula);
 app.get("/estudantes/nome", buscarEstudantePorNome);
 
-const buscarLivroPorAno = require("./livros/buscarLivroPorAno");
-app.get("/livros/buscar/ano", buscarLivroPorAno);
-
-const buscarLivroPorAutor = require("./livros/buscarLivroPorAutor");
-app.get("/livros/buscar/autor", buscarLivroPorAutor);
-
-const buscarLivroPorGenero = require("./livros/buscarLivroPorGenero");
-app.get("/livros/buscar/genero", buscarLivroPorGenero);
-
-const buscarLivroPorTitulo = require("./livros/buscarLivroPorTitulo");
-app.get("/livros/buscar/titulo", buscarLivroPorTitulo);
-
-const buscarAlugueisPorIdEstudante = require("./aluguel/buscarAluguelPorIdDoEstudante");
-app.get("/aluguel/estudante/:idEstudante", buscarAlugueisPorIdEstudante);
-
-const buscarAluguelPorIdLivro = require("./aluguel/buscarAluguelPorIdDoLivro");
-app.get("/aluguel/livro/:idLivro", buscarAluguelPorIdLivro);
-
-const buscarAluguelPorData = require("./aluguel/buscarAluguelPorData");
-app.get("/aluguel/data", buscarAluguelPorData);
-
-const cadastrarEstudantes = require("./estudantes/cadastrarEstudantes");
-const listarEstudantes = require("./estudantes/listarEstudantes");
-const editarEstudante = require("./estudantes/editarEstudantes");
-const removerEstudantes = require("./estudantes/removerEstudantes");
-
 app.post("/estudante/cadastrar", cadastrarEstudantes);
 app.get("/estudante/listar", listarEstudantes);
 app.put("/estudante/editar/:id", editarEstudante);
 app.delete("/estudante/remover/:id", removerEstudantes);
 
-const criarLivro = require("./livros/criarLivro");
 app.post("/livro/criar", criarLivro);
-
-const listarLivro = require("./livros/listarLivro");
 app.get("/livro/listar", listarLivro);
-
-const editarLivro = require("./livros/editarLivro");
 app.put("/livro/editar/:id", editarLivro);
-
-const deletarLivro = require("./livros/deletarLivro");
 app.delete("/livro/deletar/:id", deletarLivro);
 
+app.get("/livros/buscar/ano", buscarLivroPorAno);
+app.get("/livros/buscar/autor", buscarLivroPorAutor);
+app.get("/livros/buscar/genero", buscarLivroPorGenero);
+app.get("/livros/buscar/titulo", buscarLivroPorTitulo);
+
+app.get("/aluguel/estudante/:idEstudante", buscarAlugueisPorIdEstudante);
+app.get("/aluguel/livro/:idLivro", buscarAluguelPorIdLivro);
+app.get("/aluguel/data", buscarAluguelPorData);
+
 app.listen(port, () => {
-  console.log(`localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
